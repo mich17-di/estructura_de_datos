6fdef0ab3d0a750c1467ab01985c9b1d9e654fd5
mich17-di <lauraronderos69@gmail.com>
2025-07-17 20:51:51 -0500

    Add files via upload
    vs1.2 estructura de datos

--- formulario.py
+++ formulario.py
@@ -1,22 +1,35 @@
+#se importa la clase lista numero
+from lista_dato import lista_numeros
 
-objlista=lista_numeros
-class calculadora:
+objlista=lista_numeros()
+
+#creamos la clase calculadora
+class calculadora():
     
     def pedir_numero(self):
         numero1=input ("numero 1: ")
         numero2=input ("numero 2: ")
         return numero1, numero2
     
+#se almacenan los datos 
     def almacenarDatos(self, dato1, dato2):
         dato_numero=[dato1, dato2]
         objlista.incluir_listas(dato_numero)
-        
-calculadora=calculadora()
-dato1,dato2=calculadora.pedir_numero()
-calculadora.almacenarDatos(dato1,dato2)
 
-dato1,dato2=calculadora.pedir_numero()
-calculadora.almacenarDatos(dato1,dato2)
-
+#nuevos datos para extender la lista
+    def agregarLista(self):
+        print ("\nDigite dos nuevo datos para extender la lista: ")
+        num1,num2=self.pedirNumero()
+        nuevaLista=[num1,num2]
+        lista_numeros.insertarLista([nuevaLista])
+    
+# Se obtiene la posición en donde se desea insertar el dato ya determinado  
+    def añadirDato(self):
+        Dato1=input("\nDato 1:")
+        Dato2=input("Dato 2:")
+        sublista=([Dato1,Dato2])
+        posicion=int(input(f"\nDigite la posición en que desea insertar la sublista: ")) 
+        lista_numeros.almacenarDato(posicion,sublista)          
+        return posicion,sublista
 
 
--- lista_dato.py
+++ lista_dato.py
@@ -5,14 +5,56 @@ class lista_numeros:
     def __init__(self):
         self.lista_numeros=[]
         
-    def guardar_dato(self,dato_numero):
-        self.lista_numeros.extend(dato_numero)
-        print(self.lista_numeros)
+# Se guardan los datos anteriormente digitados
+    def guardarDatos(self,datoNumero):
+        self.lista_numeros.append(datoNumero)
+        print(f"\nLista con datos agregados anteriormente: {self.lista_numeros}")
         
-    def incluir_lista
+# Se inserta una nueva sublista para extenderla
+    def insertarLista(self,nuevaLista):
+        self.lista_numeros.extend(nuevaLista)
+        print("\nLa lista con datos extendidos es: ",self.lista_numeros)
+
+# Se insertan datos a la lista, dependiendo de la posiciónque se indique
+    def almacenarDato(self,posicion,sublista):
+        self.lista_numeros.insert(posicion,sublista)
+        print(f"\nLista con los nuevos datos insertados: {self.lista_numeros}")
+
+# Se elimina los datos de la sublista que se encuentre en la posición que se indique     
+    def eliminarDato(self,posicion,):
+        numeroEliminado=self.lista_numeros.pop(posicion)
+        print(f"\nEl dato {numeroEliminado} ha sido eliminado")
+        print(f"Lista actualizada: {self.lista_numeros}")
+           
+# Se elimina la sublista que contenga los números ingresados 
+    def remover(self,datos):
+        self.lista_numeros.remove(datos)
+        print("\nLista actualizada con datos removidos: ",self.lista_numeros)
         
+# Muestra la posición de la sublista que contenga los números ingresados        
+    def mostrarPosicion(self,datos):
+        print(f"\nLos datos {datos} se encuentran en posición: ", {self.lista_numeros.index(datos)})
+
+# Muestra la posición de la sublista que contenga los números ingresados    
+    def mostrarContador(self,datos):
+        print(f"\nLos datos {datos} se repiten {self.lista_numeros.count(datos)} veces")
+
+# Ordena las sublistas ingresadas de forma ascendente 
+    def ordenAscendente(self):
+        self.lista_numeros.sort()
+        print ("\nLa lista ordenada ascendentemente es: ", self.lista_numeros)
+
+# Ordena las sublistas ingresadas de forma descendente        
+    def ordenDescendente(self):
+        self.lista_numeros.reverse()
+        print ("\nLa lista ordenada descendentemente es: ", self.lista_numeros)
         
-        
+# Se muestra el elemento encontrado en el indice que se que se indique
+    def verNumero(self,posicion):
+        numero=self.lista_numeros.index(posicion)
+        return numero
+
+                               
         
         
         
